1eac4df87475cf8c70002b9e4d1cd6d8579c6ba7
solfany <solfany1999@gmail.com>
2023-08-28 15:15:00 +0900

    블로그 업데이트

--- src/pages/LandingPages/Author/sections/Contact.js
+++ src/pages/LandingPages/Author/sections/Contact.js
@@ -18,7 +18,6 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
 import Card from "@mui/material/Card";
-
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
 import MKInput from "components/MKInput";
--- src/pages/Presentation/sections/BoardPages/BoardPages.js
+++ src/pages/Presentation/sections/BoardPages/BoardPages.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import MKBox from "components/MKBox";
+import Chip from "@mui/material/Chip";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
 import "../css/CTableTitle.css";
+
 function BoardPages() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
@@ -41,22 +49,94 @@ function BoardPages() {
       date: "2023-09-17",
       author: "김솔비",
     },
+    {
+      id: "5",
+      title: "네 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
+    {
+      id: "6",
+      title: "네 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
+    {
+      id: "7",
+      title: "네 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
+    {
+      id: "8",
+      title: "네 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
+    {
+      id: "9",
+      title: "네 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
+    {
+      id: "10",
+      title: "10 번째 게시글",
+      content: "두 번째 게시글의 내용입니다.",
+      tags: ["안녕", "primary", "error"],
+      date: "2023-09-17",
+      author: "김솔비",
+    },
   ];
 
   useEffect(() => {
-    // postId를 숫자로 변환해서 비교
     const currentPost = samplePosts.find((p) => parseInt(p.id) === parseInt(id));
     setPost(currentPost);
   }, [id]);
+
   if (!post) return <div>게시글을 찾을 수 없습니다.</div>;
 
   return (
     <MKBox component="section" py={10}>
       <Container>
-        <h1>{post.title}</h1>
-        <p>{post.content}</p>
+        <Typography variant="h3" gutterBottom>
+          {post.title}
+        </Typography>
+        <Box display="flex" alignItems="center" marginBottom={3}>
+          <Avatar>{post.author[0]}</Avatar>
+          <Box marginLeft={2}>
+            <Typography variant="subtitle1">{post.author}</Typography>
+            <Typography variant="caption" color="textSecondary">
+              {post.date}
+            </Typography>
+          </Box>
+        </Box>
+        <Divider style={{ marginBottom: "20px" }} />
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="body1" paragraph>
+              {post.content}
+            </Typography>
+          </CardContent>
+          <Box p={2} display="flex" flexDirection="row" gap={1}>
+            {post.tags.map((tag, index) => (
+              <Chip key={index} label={tag} variant="outlined" />
+            ))}
+          </Box>
+        </Card>
       </Container>
     </MKBox>
   );
 }
+
 export default BoardPages;
